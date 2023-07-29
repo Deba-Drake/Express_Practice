62a4091c331fe60b4a528251ea532fb9f39f35d3
Deba-Drake <debayandas.185@gmail.com>
2023-07-29 21:40:28 +0530

    Added Schema and model

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000;
 dotenv.config({ path: "./config.env" });
 // console.log(process.env);
 
+//DATABASE
 //Connecting Express and MongoDB
-
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
@@ -17,6 +17,23 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+//Creating and Validating a Database Schema
+const tour_schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour should be specified"],
+    unique: true,
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    required: [true, "A price should be specified"],
+  },
+});
+//Creating a Database Model
+const tour = mongoose.model("tour", tour_schema);
+
 //Server
 //to Listen as the port starts
 app.listen(port, "127.0.0.1", () => {
