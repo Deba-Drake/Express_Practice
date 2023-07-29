c5b1f7e1acef0accb4cb469f74dca08d142aff55
Deba-Drake <debayandas.185@gmail.com>
2023-07-29 15:06:27 +0530

    Connected with MongoDB

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
 const port = process.env.PORT || 3000;
@@ -5,6 +6,17 @@ const port = process.env.PORT || 3000;
 dotenv.config({ path: "./config.env" });
 // console.log(process.env);
 
+//Connecting Express and MongoDB
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((error) => {
+    console.log(error);
+  });
 //Server
 //to Listen as the port starts
 app.listen(port, "127.0.0.1", () => {
